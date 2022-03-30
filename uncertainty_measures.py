01681a26d6a9f20444e1b37600f8cc5f59f131bd
Tiago Miguel Saldanha Salvador <tsalda@campus.MCGILL.CA@math-aogpu2>
2022-03-30 15:51:06 -0400

    add utils uncertainty_measures

---
+++ uncertainty_measures.py
@@ -0,0 +1,136 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+from scipy import optimize
+import numpy as np
+
+from tqdm import tqdm
+
+def get_ks(confidences, ground_truth):
+    n = len(ground_truth)
+    order_sort = np.argsort(confidences)
+    ks = np.max(np.abs(np.cumsum(confidences[order_sort])/n-np.cumsum(ground_truth[order_sort])/n))
+    return ks
+
+
+def get_ks_l1(confidences, ground_truth):
+    n = len(ground_truth)
+    order_sort = np.argsort(confidences)
+    ks = np.mean(np.abs(np.cumsum(confidences[order_sort])/n-np.cumsum(ground_truth[order_sort])/n))
+    return ks
+
+
+def get_brier(confidences, ground_truth):
+    # Compute Brier
+    brier = np.zeros(confidences.shape)
+    brier[ground_truth] = (1-confidences[ground_truth])**2
+    brier[np.logical_not(ground_truth)] = (confidences[np.logical_not(ground_truth)])**2
+    brier = np.mean(brier)
+    return brier
+
+
+def bin_confidences_and_accuracies(confidences, ground_truth, bin_edges, indices):
+    i = np.arange(0, bin_edges.size-1)
+    aux = indices == i.reshape((-1, 1))
+    counts = aux.sum(axis=1)
+    weights = counts / np.sum(counts)
+    correct = np.logical_and(aux, ground_truth).sum(axis=1)
+    a = np.repeat(confidences.reshape(1, -1), bin_edges.size-1, axis=0)
+    a[np.logical_not(aux)] = 0
+    bin_accuracy = correct / counts
+    bin_confidence = a.sum(axis=1) / counts
+    return weights, bin_accuracy, bin_confidence
+
+
+def get_ece(confidences, ground_truth, nbins):
+    # Repeated code from determine edges. Here it is okay if the bin edges are not unique defined
+    confidences_sorted = confidences.copy()
+    confidences_index = confidences.argsort()
+    confidences_sorted = confidences_sorted[confidences_index]
+    aux = np.linspace(0, len(confidences_sorted) - 1, nbins + 1).astype(int) + 1
+    bin_indices = np.zeros(len(confidences_sorted)).astype(int)
+    bin_indices[:aux[1]] = 0
+    for i in range(1, len(aux) - 1):
+        bin_indices[aux[i]:aux[i + 1]] = i
+    bin_edges = np.zeros(nbins + 1)
+    for i in range(0, nbins - 1):
+        bin_edges[i + 1] = np.mean(np.concatenate((
+            confidences_sorted[bin_indices == i][confidences_sorted[bin_indices == i] == max(confidences_sorted[bin_indices == i])],
+            confidences_sorted[bin_indices == (i + 1)][
+                confidences_sorted[bin_indices == (i + 1)] == min(confidences_sorted[bin_indices == (i + 1)])])))
+    bin_edges[0] = 0
+    bin_edges[-1] = 1
+    bin_indices = bin_indices[np.argsort(confidences_index)]
+
+    weights, bin_accuracy, bin_confidence = bin_confidences_and_accuracies(confidences, ground_truth, bin_edges,
+                                                                           bin_indices)
+    ece = np.dot(weights, np.abs(bin_confidence - bin_accuracy))
+    return ece
+
+
+def compute_scores(confidences, ground_truth, nbins):
+
+    # Compute ECE
+    ece = get_ece(confidences, ground_truth, nbins)
+
+    # Compute Brier
+    brier = get_brier(confidences, ground_truth)
+
+    # Compute KS
+    ks = get_ks(confidences, ground_truth)
+
+    # Compute KS-L1
+    ks_l1 = get_ks_l1(confidences, ground_truth)
+
+    return ece, ks, ks_l1, brier
+
+def compute_classwise_scores(softmaxes, labels, nbins):
+    total = 0
+    ece = 0
+    ks = 0
+    ks_l1 = 0
+    brier = 0
+    for k in range(softmaxes.shape[1]):
+        select = labels==k
+        n_k = torch.sum(select).item()
+        ece_temp, ks_temp, ks_l1_temp, brier_temp = compute_scores(softmaxes[:,k].numpy(), select.numpy(), nbins)
+        ece += n_k*ece_temp
+        ks += n_k*ks_temp
+        ks_l1 += n_k*ks_l1_temp
+        brier += n_k*brier_temp
+        total += n_k
+    ece /= total
+    ks /= total
+    ks_l1 /= total
+    brier /= total
+    return ece, ks, ks_l1, brier
+
+
+def get_uncertainty_measures(logits, labels):
+    results = {}
+    
+    softmaxes = nn.Softmax(dim=1)(logits)
+    # Compute Accuracy
+    confidences, predictions = torch.max(softmaxes, 1)
+    accuracies = predictions.eq(labels)
+    results['acc'] = accuracies.float().mean().item()
+
+    # Top-1 ECE equal mass (with nbins=15), Brier Score, KS
+    confidences = confidences.numpy()
+    accuracies = accuracies.numpy()
+    results['top1_ece_eq_mass'], results['top1_ks'], results['top1_ks_l1'], results['top1_brier'] = compute_scores(confidences, accuracies, 15)
+    
+    # Classwise ECE
+    results['cw_ece_eq_mass'], results['cw_ks'], results['cw_ks_l1'], results['cw_brier'] = compute_classwise_scores(softmaxes, labels, 15)
+    
+    # NLL (Negative Log-Likelihood)
+    nll_criterion = nn.CrossEntropyLoss(reduction='none')
+    results['nll'] = torch.mean(nll_criterion(logits, labels)).item()
+    
+    ## Transform to onehot encoded labels
+    labels_onehot = torch.FloatTensor(logits.shape[0], logits.shape[1])
+    labels_onehot.zero_()
+    labels_onehot.scatter_(1, labels.long().view(len(labels), 1), 1)
+    results['brier'] = torch.mean(torch.sum((softmaxes - labels_onehot) ** 2, dim=1,keepdim = True)).item()
+    
+    return results
---
+++ utils.py
@@ -0,0 +1,100 @@
+from pytorchcv.model_provider import get_model as ptcv_get_model
+from tqdm import tqdm
+from imgclsmob.pytorch.utils import prepare_model as prepare_model_pt
+
+import torch
+from torch import nn
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+
+import numpy as np
+import os
+
+from scipy.optimize import minimize
+from sklearn import linear_model
+import scipy.stats
+
+from uncertainty_measures import get_uncertainty_measures
+
+def create_folder(path):
+    try:
+        os.mkdir(path)
+    except OSError:
+        pass
+
+def small_large_split(y_data, nsamples, ds_info):
+    indices_small = np.zeros(len(y_data), dtype=bool)
+    indices_large = np.zeros(len(y_data), dtype=bool)
+    for y in range(ds_info['num_classes']):
+        select = y_data==y
+        indices_y = np.arange(len(y_data))[select]
+        choice = np.random.choice(range(len(indices_y)), size=nsamples//ds_info['num_classes'], replace=False)    
+        ind = np.zeros(len(indices_y), dtype=bool)
+        ind[choice] = True
+        rest = ~ind
+        indices_small[indices_y[ind]] = True
+        indices_large[indices_y[rest]] = True
+    return indices_small, indices_large
+
+
+def get_loader(subset, ds_info, shuffle=False, transform=None):
+    if subset != 'train':
+        indices = ds_info['indices_'+subset]
+    if ds_info['name'] == 'cifar10':
+        if transform is None:
+            transform = ds_info['transform']
+        dataset = torchvision.datasets.CIFAR10(root=ds_info['root_folder_datasets'], train=subset=='train',
+                                               download=False, transform=transform);
+        if subset != 'train':
+            dataset.data = dataset.data[indices]
+            dataset.targets = np.array(dataset.targets)[indices]
+    elif ds_info['name'] == 'imagenet':
+        if subset == 'train':
+            dataset_dir = os.path.join(ds_info['root_folder_datasets'], 'train')
+        elif subset == 'train_large':
+            dataset_dir = os.path.join(ds_info['root_folder_datasets'], 'train')
+        else:
+            dataset_dir = os.path.join(ds_info['root_folder_datasets'], 'val')
+        dataset = torchvision.datasets.ImageFolder(dataset_dir, transform=ds_info['transform'])
+        dataset.samples = np.array(dataset.samples)[indices]
+        dataset.targets = np.array(dataset.targets)[indices]
+    loader = torch.utils.data.DataLoader(dataset, batch_size=ds_info['batch_size'], shuffle=shuffle, num_workers=4)
+    return loader
+
+
+def get_features_logits_labels(net, subset, ds_info, save=True):
+    experiments_folder = ds_info['folder']
+    suffix_name = '.npy'
+    folder_path = os.path.join(experiments_folder,'features_logits_labels')
+    if os.path.exists(os.path.join(folder_path, 'labels_'+subset+suffix_name)):
+        features = torch.from_numpy(np.load(os.path.join(folder_path, 'features_'+subset+suffix_name)))
+        logits = torch.from_numpy(np.load(os.path.join(folder_path, 'logits_'+subset+suffix_name)))
+        labels = torch.from_numpy(np.load(os.path.join(folder_path, 'labels_'+subset+suffix_name)))
+    else:
+        dataloader = get_loader(subset, ds_info)
+        net.eval()
+        features = torch.zeros(len(dataloader.dataset.targets), ds_info['dim_features'])
+        logits = torch.zeros(len(dataloader.dataset.targets), ds_info['num_classes'])
+        start = 0
+        end = 0
+        with torch.no_grad():
+            suffix_message = " for clean"
+            for data in tqdm(dataloader, desc=f"Generating features, logits and labels"+suffix_message, leave=False):
+                images, labels = data
+                end += len(images)
+                # compute features and logits
+                features_temp = net.feature_extractor(images.cuda())
+                logits[start:end] = net.output(features_temp).cpu()
+                if 'squeezenet' in ds_info['architecture']:
+                    features[start:end] = features_temp.cpu().reshape(features_temp.shape[0],-1)
+                else:
+                    features[start:end] = features_temp.cpu()            
+                start = end
+        labels = torch.from_numpy(dataloader.dataset.targets)
+        if save:
+            np.save(os.path.join(folder_path, 'features_'+subset+suffix_name), features)
+            np.save(os.path.join(folder_path, 'logits_'+subset+suffix_name), logits)
+            np.save(os.path.join(folder_path, 'labels_'+subset+suffix_name), labels)    
+
+    return features, logits, labels
